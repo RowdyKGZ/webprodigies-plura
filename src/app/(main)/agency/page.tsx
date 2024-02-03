d4d3d2eb4d1b4e00acc2b1d9ed1e948a8fc90bcf
RowdyKGZ <rowdykg@gmail.com>
2024-02-03 13:44:38 +0600

    agency section

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,5 +1,58 @@
-const Page = () => {
-  return <div>Agency</div>;
+import { Plan } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+
+import { AgencyDetails } from "@/components/forms/agency-details";
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { plan: Plan; state: string; code: string };
+}) => {
+  const agencyId = await verifyAndAcceptInvitation();
+
+  const user = await getAuthUserDetails();
+
+  if (agencyId) {
+    if (user?.role === "SUBACCOUNT_USER" || user?.role === "SUBACCOUNT_GUEST") {
+      return redirect("/subaccount");
+    } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      if (searchParams.plan) {
+        return redirect(
+          `/agency/${agencyId}/billing?plan=${searchParams.plan}`
+        );
+      }
+      if (searchParams.state) {
+        const statePath = searchParams.state.split("__")[0];
+        const stateAgencyId = searchParams.state.split("___")[1];
+
+        if (!stateAgencyId) return <div>Not Authorized</div>;
+
+        return redirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`
+        );
+      } else {
+        return redirect(`/agency/${agencyId}`);
+      }
+    } else {
+      return <div>Not Authorized</div>;
+    }
+  }
+
+  const authUser = await currentUser();
+  if (!authUser) redirect("/sign-in");
+
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <div className="max-w-[850px] border-[10px] p-4 rounded-xl">
+        <h1 className="text-3xl">Create An Agency</h1>
+        <AgencyDetails
+          data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
