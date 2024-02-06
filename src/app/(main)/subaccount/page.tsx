e899f10a9bce15bdab1c178fc7e6aacd206c56a2
RowdyKGZ <rowdykg@gmail.com>
2024-02-06 13:56:42 +0600

    subaccount section

--- src/app/(main)/subaccount/page.tsx
+++ src/app/(main)/subaccount/page.tsx
@@ -1,7 +1,39 @@
-type Props = {};
+import Unauthorized from "@/components/unauthorized";
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { redirect } from "next/navigation";
 
-const SubaccountPage = (props: Props) => {
-  return <div>SubaccountPage</div>;
+type Props = {
+  searchParams: { state: string; code: string };
+};
+
+const SubaccountPage = async ({ searchParams }: Props) => {
+  const agencyId = await verifyAndAcceptInvitation();
+  if (!agencyId) return <Unauthorized />;
+
+  const user = await getAuthUserDetails();
+  if (!user) return;
+
+  const getFirstSubaccountWitchAccess = user.Permissions.find(
+    (permission) => permission.access === true
+  );
+
+  if (searchParams.state) {
+    const statePath = searchParams.state.split("___")[0];
+    const stateSubaccountId = searchParams.state.split("___")[1];
+    if (!stateSubaccountId) return <Unauthorized />;
+
+    return redirect(
+      `/subaccount/${stateSubaccountId}/${statePath}?code=${searchParams.code}`
+    );
+  }
+
+  if (getFirstSubaccountWitchAccess) {
+    return redirect(
+      `/subaccount/${getFirstSubaccountWitchAccess.subAccountId}`
+    );
+  }
+
+  return <Unauthorized />;
 };
 
 export default SubaccountPage;
--- src/components/global/infobar.tsx
+++ src/components/global/infobar.tsx
@@ -36,7 +36,7 @@ export const Infobar = ({
   const [allNotifications, setAllNotifications] = useState(notifications);
   const [showAll, setShowAll] = useState(true);
 
-  const hadleClick = () => {
+  const handleClick = () => {
     if (!showAll) {
       setAllNotifications(notifications);
     } else {
@@ -55,60 +55,57 @@ export const Infobar = ({
     <>
       <div
         className={twMerge(
-          "fixed z-[20] md:left-[300px] left-0 right-0 top-0 p-4 bg-background/80 backdrop-blur-md flex gap-4 items-center border-b-[1px]",
+          "fixed z-[20] md:left-[300px] left-0 right-0 top-0 p-4 bg-background/80 backdrop-blur-md flex  gap-4 items-center border-b-[1px] ",
           className
         )}
       >
         <div className="flex items-center gap-2 ml-auto">
           <UserButton afterSignOutUrl="/" />
-
           <Sheet>
-            <SheetTrigger className="rounded-full w-9 h-9 flex items-center justify-center text-white">
-              <Bell size={17} />
+            <SheetTrigger>
+              <div className="rounded-full w-9 h-9 bg-primary flex items-center justify-center text-white">
+                <Bell size={17} />
+              </div>
             </SheetTrigger>
-
-            <SheetContent className="mt-4 mr-4 pr-4 flex flex-col">
+            <SheetContent className="mt-4 mr-4 pr-4 overflow-scroll">
               <SheetHeader className="text-left">
                 <SheetTitle>Notifications</SheetTitle>
                 <SheetDescription>
                   {(role === "AGENCY_ADMIN" || role === "AGENCY_OWNER") && (
                     <Card className="flex items-center justify-between p-4">
-                      Current subaccount
-                      <Switch onChangeCapture={hadleClick} />
+                      Current Subaccount
+                      <Switch onCheckedChange={handleClick} />
                     </Card>
                   )}
                 </SheetDescription>
               </SheetHeader>
-
               {allNotifications?.map((notification) => (
                 <div
                   key={notification.id}
-                  className="flex flex-col gap-y-2 mb-2 overflow-x-scroll text-ellipsis"
+                  className="flex flex-col gap-y-2 mb-5 text-ellipsis"
                 >
                   <div className="flex gap-2">
                     <Avatar>
                       <AvatarImage
                         src={notification.User.avatarUrl}
-                        alt="Profile"
+                        alt="Profile Picture"
                       />
                       <AvatarFallback className="bg-primary">
-                        {notification.User.name.slice(0, 2).toLocaleUpperCase()}
+                        {notification.User.name.slice(0, 2).toUpperCase()}
                       </AvatarFallback>
                     </Avatar>
-
                     <div className="flex flex-col">
                       <p>
                         <span className="font-bold">
                           {notification.notification.split("|")[0]}
                         </span>
-                        <span className="font-text-muted-foreground">
+                        <span className="text-muted-foreground">
                           {notification.notification.split("|")[1]}
                         </span>
                         <span className="font-bold">
                           {notification.notification.split("|")[2]}
                         </span>
                       </p>
-
                       <small className="text-xs text-muted-foreground">
                         {new Date(notification.createdAt).toLocaleDateString()}
                       </small>
@@ -116,15 +113,16 @@ export const Infobar = ({
                   </div>
                 </div>
               ))}
-
               {allNotifications?.length === 0 && (
-                <div className="flex items-center justify-center mb-4 text-muted-foreground">
-                  You hane no notifications
+                <div
+                  className="flex items-center justify-center text-muted-foreground"
+                  mb-4
+                >
+                  You have no notifications
                 </div>
               )}
             </SheetContent>
           </Sheet>
-
           <ModeToggle />
         </div>
       </div>
