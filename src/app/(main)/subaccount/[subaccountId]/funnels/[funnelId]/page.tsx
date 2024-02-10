5dc502c0a01ffc43ba9a491a805f9b76ed69e5a1
RowdyKGZ <rowdykg@gmail.com>
2024-02-10 21:48:58 +0600

    funnel end

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/page.tsx
@@ -5,6 +5,7 @@ import { getFunnel } from "@/lib/queries";
 import BlurPage from "@/components/global/blur-page";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import FunnelSettings from "./_components/funnel-settings";
+import FunnelSteps from "./_components/funnel-steps";
 
 type Props = {
   params: {
